90d24f750a8d6127fc71b0a145d5113eb04a2263
JayC <caballerojavier13@gmail.com>
2023-09-17 20:01:01 -0300

    Patch user

--- server/src/routes/users/index.ts
+++ server/src/routes/users/index.ts
@@ -1,8 +1,10 @@
+import { errorCodes } from 'fastify'
 import { Static, Type } from "@sinclair/typebox";
 import * as gavatar from "gravatar";
 import firebaseApp from "@/services/firebase";
 import UserSchema from "@/models/user.model";
 import AuthMiddleware from "@/middlewares/auth.middleware";
+import CountryModel from "@/models/country.model";
 export const User = Type.Object({
   id: Type.Readonly(Type.String()),
   name: Type.Required(Type.String()),
@@ -70,6 +72,46 @@ const users: any = async (fastify: any): Promise<void> => {
       user.lang = newData.lang;
       user.save();
       reply.status(201).send({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar || null,
+        lang: user.lang || null,
+        country: user.country.name
+      });
+    }
+  );
+  fastify.patch(
+    '/me', {
+      schema: {
+        body: {
+          type: 'object',
+          properties: {
+            country: {
+              type: 'string'
+            },
+            lang: {
+              type: 'string'
+            }
+          },
+          additionalProperties: false
+        },
+        response: {
+          200: User
+        },
+      },
+    },
+    async function (request: any, reply: any) {
+      const { country: countryName, lang } = request.body;
+      const country = await CountryModel.findOne({name: countryName});
+      if (!country) {
+        throw new errorCodes.FST_ERR_NOT_FOUND('Country');
+      }
+      const user = request.user;
+      user.country = country;
+      user.lang = lang;
+      user.save();
+      reply.status(200).send({
         id: user.id,
         name: user.name,
         email: user.email,
