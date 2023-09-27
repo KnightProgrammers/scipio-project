4afb2d61b7222eb8371afedde59dd103901faa7b
JayC <caballerojavier13@gmail.com>
2023-09-27 18:07:41 -0300

    Client: Add locators

--- client/src/components/template/UserDropdown.tsx
+++ client/src/components/template/UserDropdown.tsx
@@ -36,6 +36,7 @@ const _UserDropdown = ({ className }: CommonProps) => {
             <Avatar
                 size={32}
                 shape="circle"
+                data-tn="header-user-avatar"
                 src={
                     userState.avatar ?? './img/thumbs/generic-user-profile.png'
                 }
@@ -53,12 +54,12 @@ const _UserDropdown = ({ className }: CommonProps) => {
             >
                 <Dropdown.Item variant="header">
                     <div className="py-2 px-3 flex items-center gap-2">
-                        <Avatar shape="circle" src={userState.avatar} />
+                        <Avatar shape="circle" src={userState.avatar} data-tn="profile-user-avatar"/>
                         <div>
-                            <div className="font-bold text-gray-900 dark:text-gray-100">
+                            <div className="font-bold text-gray-900 dark:text-gray-100" data-tn="profile-user-name">
                                 {userState.name}
                             </div>
-                            <div className="text-xs">{userState.email}</div>
+                            <div className="text-xs" data-tn="profile-user-email">{userState.email}</div>
                         </div>
                     </div>
                 </Dropdown.Item>
