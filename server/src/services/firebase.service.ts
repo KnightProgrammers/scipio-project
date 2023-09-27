1cd25fedab2ce9c2ac2fb14882e55979c797b8f2
JayC <caballerojavier13@gmail.com>
2023-09-27 18:08:01 -0300

    Server: Fix private file parse

--- server/src/services/firebase.service.ts
+++ server/src/services/firebase.service.ts
@@ -3,7 +3,7 @@ import {ServiceAccount} from "firebase-admin";
 
 const serviceAccount: ServiceAccount = {
     "projectId": process.env.FIREBASE_PROJECT_ID,
-    "privateKey": process.env.FIREBASE_PRIVATE_KEY,
+    "privateKey": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
   }
 ;
