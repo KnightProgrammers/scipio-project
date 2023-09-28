033a12dfd701efef1b4763d1646b1cdda98ba94b
JayC <caballerojavier13@gmail.com>
2023-09-28 17:37:22 -0300

    Test: Better reporter

--- tests/e2e-tests/playwright.config.ts
+++ tests/e2e-tests/playwright.config.ts
@@ -20,7 +20,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', { open: 'never', outputFolder: 'playwright-report' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: !!process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:5173/',
