14bc380e13e43ffa5accc6f432e7ccbffcecc25e
Javier Caballero <caballerojavier13@gmail.com>
2023-10-04 18:07:40 +0000

    DB-Seed: Add linter

--- db-seed/src/helpers/logger.ts
+++ db-seed/src/helpers/logger.ts
@@ -1,24 +1,21 @@
-import pino from "pino";
-import pretty from "pino-pretty";
+import pino from 'pino';
+import pretty from 'pino-pretty';
 
 const stream = pretty({
-  colorize: true,
-})
+	colorize: true,
+});
 
-const LOG_LEVELS = [
-  'trace',
-  'debug',
-  'info',
-  'warn',
-  'error'
-]
+const LOG_LEVELS = ['trace', 'debug', 'info', 'warn', 'error'];
 
 const logLevel: string = (process.env.LOG_LEVEL || '').toLowerCase();
 
-const logger = pino({
-  msgPrefix: '',
-  base: null,
-  level: LOG_LEVELS.includes(logLevel) ? logLevel : 'info'
-},stream)
+const logger = pino(
+	{
+		msgPrefix: '',
+		base: null,
+		level: LOG_LEVELS.includes(logLevel) ? logLevel : 'info',
+	},
+	stream,
+);
 
 export default logger;
--- db-seed/src/index.ts
+++ db-seed/src/index.ts
@@ -1,42 +1,52 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import logger from "./helpers/logger";
-import countrySeed from "./seeds/country";
-import currencySeed from "./seeds/currency";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import logger from './helpers/logger';
+import countrySeed from './seeds/country';
+import currencySeed from './seeds/currency';
 
 dotenv.config();
 
 const config = {
-  db: {
-    protocol: process.env.MONGO_DB_PROTOCOL,
-    host: process.env.MONGO_DB_HOST,
-    port: process.env.MONGO_DB_PORT,
-    name: process.env.MONGO_DB_NAME,
-    params: process.env.MONGO_DB_PARAMS,
-    user: process.env.MONGO_DB_USER,
-    password: process.env.MONGO_DB_PASSWORD,
-  }
-};
+	db: {
+		protocol: process.env.MONGO_DB_PROTOCOL,
+		host: process.env.MONGO_DB_HOST,
+		port: process.env.MONGO_DB_PORT,
+		name: process.env.MONGO_DB_NAME,
+		params: process.env.MONGO_DB_PARAMS,
+		user: process.env.MONGO_DB_USER,
+		password: process.env.MONGO_DB_PASSWORD,
+	},
+}
 
-(async function main() {
-  logger.info("Connecting to DB")
-  logger.info(`  ${config.db.protocol}://${config.db.host}${config.db.port ? `:${config.db.port}` : ''}/${!!config.db.name && config.db.name}${!!config.db.params ? `?${config.db.params}` : ''}`)
+;(async function main() {
+	logger.info('Connecting to DB');
+	logger.info(
+		`  ${config.db.protocol}://${config.db.host}${
+			config.db.port ? `:${config.db.port}` : ''
+		}/${!!config.db.name && config.db.name}${
+			config.db.params ? `?${config.db.params}` : ''
+		}`,
+	);
 
-  const dbConnection = await mongoose.connect(
-    `${config.db.protocol}://${config.db.host}${config.db.port ? `:${config.db.port}` : ''}/${!!config.db.name && config.db.name}${!!config.db.params ? `?${config.db.params}` : ''}`,
-    {
-      user: config.db.user,
-      pass: config.db.password,
-      bufferCommands: false,
-      autoCreate: true
-    }
-  );
-  logger.info('DB connection is up');
-  logger.info('-----------------');
-  logger.info('Starting seeding:');
-  await countrySeed();
-  await currencySeed();
-  logger.info('-----------------');
-  await dbConnection.disconnect();
-  logger.info('Done!')
+	const dbConnection = await mongoose.connect(
+		`${config.db.protocol}://${config.db.host}${
+			config.db.port ? `:${config.db.port}` : ''
+		}/${!!config.db.name && config.db.name}${
+			config.db.params ? `?${config.db.params}` : ''
+		}`,
+		{
+			user: config.db.user,
+			pass: config.db.password,
+			bufferCommands: false,
+			autoCreate: true,
+		},
+	);
+	logger.info('DB connection is up');
+	logger.info('-----------------');
+	logger.info('Starting seeding:');
+	await countrySeed();
+	await currencySeed();
+	logger.info('-----------------');
+	await dbConnection.disconnect();
+	logger.info('Done!');
 })();
