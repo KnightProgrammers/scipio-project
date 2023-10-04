2cb6d2ffc7b555b4d68ad780272c59eaa0c0f1b5
Javier Caballero <caballerojavier13@gmail.com>
2023-10-04 18:34:02 +0000

    Infra: Add linter conf

--- infra/src/commands/generate-pr-comment.command.js
+++ infra/src/commands/generate-pr-comment.command.js
@@ -3,69 +3,69 @@ import fs from 'fs';
 import toTitleCase from '../helpers/to-title-case.js';
 import RailwayService from '../services/railway.service.js';
 
-
 const railwayClient = new RailwayService(process.env.RAILWAY_PROJECT_ID);
 
 const generatePrCommentCommand = async (options) => {
-    console.log(
-        figlet.textSync(`Generate PR Comment`, {
-            font: 'AMC 3 Line'
-        })
-    )
+	console.log(
+		figlet.textSync('Generate PR Comment', {
+			font: 'AMC 3 Line',
+		}),
+	);
 
-    let content = '## Deploy Status';
+	let content = '## Deploy Status';
 
-    // Add link to dashboard
-    const projectId = process.env.RAILWAY_PROJECT_ID;
-    content += `\n> Railway Dashboard: [link](https://railway.app/project/${projectId})`;
-    const environmentName = options.environment.toLowerCase();
-    const environments = await railwayClient.getEnvironments();
+	// Add link to dashboard
+	const projectId = process.env.RAILWAY_PROJECT_ID;
+	content += `\n> Railway Dashboard: [link](https://railway.app/project/${projectId})`;
+	const environmentName = options.environment.toLowerCase();
+	const environments = await railwayClient.getEnvironments();
 
-    const environment = environments.find(({ name }) => name === environmentName);
+	const environment = environments.find(
+		({ name }) => name === environmentName,
+	);
 
-    if (!environment) {
-        throw new Error(`Environment "${environmentName}" not found`)
-    }
+	if (!environment) {
+		throw new Error(`Environment "${environmentName}" not found`);
+	}
 
-    const services = await railwayClient.getServices();
+	const services = await railwayClient.getServices();
 
-    let hadJobFailed = false;
+	let hadJobFailed = false;
 
-    for (const service of services) {
-        const deployment = await railwayClient.getLatestDeployment({
-            first: 1,
-            serviceId: service.id,
-            environmentId: environment.id
-        });
+	for (const service of services) {
+		const deployment = await railwayClient.getLatestDeployment({
+			first: 1,
+			serviceId: service.id,
+			environmentId: environment.id,
+		});
 
-        content += '\n\n';
-        content += `### ${toTitleCase(service.name.split('scipio-')[1])}`
-        content += '\n';
+		content += '\n\n';
+		content += `### ${toTitleCase(service.name.split('scipio-')[1])}`;
+		content += '\n';
 
-        const DEPLOY_STATUS_LABEL = {
-            CRASHED: 'Crashed ❌',
-            FAILED: 'Failed ❌',
-            REMOVED: 'Removed 🗑️',
-            SKIPPED: 'Skipped 🧸',
-            SUCCESS: 'Success ✅'
-        }
+		const DEPLOY_STATUS_LABEL = {
+			CRASHED: 'Crashed ❌',
+			FAILED: 'Failed ❌',
+			REMOVED: 'Removed 🗑️',
+			SKIPPED: 'Skipped 🧸',
+			SUCCESS: 'Success ✅',
+		};
 
-        if (['CRASHED', 'FAILED'].includes(deployment.status)) {
-            hadJobFailed = true;
-        }
+		if (['CRASHED', 'FAILED'].includes(deployment.status)) {
+			hadJobFailed = true;
+		}
 
-        content += ` - **Status:** ${DEPLOY_STATUS_LABEL[deployment.status]} `
-        content += '\n';
-        content += ` - **Url:** [${service.name}-${environmentName}.up.railway.app 🔗]`
-        content += `(https://${service.name}-${environmentName}.up.railway.app)`
-    }
+		content += ` - **Status:** ${DEPLOY_STATUS_LABEL[deployment.status]} `;
+		content += '\n';
+		content += ` - **Url:** [${service.name}-${environmentName}.up.railway.app 🔗]`;
+		content += `(https://${service.name}-${environmentName}.up.railway.app)`;
+	}
 
-    fs.writeFileSync('./pr-comment.md', content);
+	fs.writeFileSync('./pr-comment.md', content);
 
-    if (hadJobFailed) {
-        throw new Error('Deploy failed');
-    }
-
-}
+	if (hadJobFailed) {
+		throw new Error('Deploy failed');
+	}
+};
 
 export default generatePrCommentCommand;
--- infra/src/helpers/to-title-case.js
+++ infra/src/helpers/to-title-case.js
@@ -1,14 +1,10 @@
 const toTitleCase = (str) => {
-    if ((str === null) || (str === ''))
-        return false;
-    else
-        str = str.toString();
- 
-    return str.replace(/\w\S*/g,
-        function (txt) {
-            return txt.charAt(0).toUpperCase() +
-                txt.substr(1).toLowerCase();
-        });
-}
+	if (str === null || str === '') return false;
+	else str = str.toString();
+
+	return str.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+};
 
 export default toTitleCase;
