5a1ed21a231d7d54c91740eb4f0cc712037d664a
Javier Caballero <caballerojavier13@gmail.com>
2023-10-04 19:20:59 +0000

    Remove prettier

--- db-seed/src/seeds/country.ts
+++ db-seed/src/seeds/country.ts
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose'
-import logger from '../helpers/logger'
+import mongoose from 'mongoose';
+import logger from '../helpers/logger';
 
-import { CountrySchema } from '../../../server/src/models/country.model'
-import countryList from '../fixtures/countries.json'
+import { CountrySchema } from '../../../server/src/models/country.model';
+import countryList from '../fixtures/countries.json';
 
 export default async () => {
-    const CountryModel = mongoose.model('Country', CountrySchema)
-    logger.info(' • Seeding Countries')
-    for (const { name, code, isSupported = false } of countryList) {
-        try {
-            const exist = await CountryModel.findOne({ name })
-            if (exist) {
-                logger.debug(`\t "${name}" already exists`)
-            } else {
-                await CountryModel.create({ name, code, isSupported })
-                logger.info(`\t "${name}" added`)
-            }
-        } catch (e) {
-            console.error(e)
-        }
-    }
-}
+	const CountryModel = mongoose.model('Country', CountrySchema);
+	logger.info(' • Seeding Countries');
+	for (const { name, code, isSupported = false } of countryList) {
+		try {
+			const exist = await CountryModel.findOne({ name });
+			if (exist) {
+				logger.debug(`\t "${name}" already exists`);
+			} else {
+				await CountryModel.create({ name, code, isSupported });
+				logger.info(`\t "${name}" added`);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
+};
--- db-seed/src/seeds/currency.ts
+++ db-seed/src/seeds/currency.ts
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose'
-import logger from '../helpers/logger'
+import mongoose from 'mongoose';
+import logger from '../helpers/logger';
 
-import { CurrencySchema } from '../../../server/src/models/currency.model'
-import currencyList from '../fixtures/currencies.json'
+import { CurrencySchema } from '../../../server/src/models/currency.model';
+import currencyList from '../fixtures/currencies.json';
 
 export default async () => {
-    const CurrencyModel = mongoose.model('Currency', CurrencySchema)
-    logger.info(' • Seeding Currencies')
-    for (const { name, code } of currencyList) {
-        try {
-            const exist = await CurrencyModel.findOne({ code })
-            if (exist) {
-                logger.debug(`\t "${code}" already exists`)
-            } else {
-                await CurrencyModel.create({ name, code })
-                logger.info(`\t "${code}" added`)
-            }
-        } catch (e) {
-            console.error(e)
-        }
-    }
-}
+	const CurrencyModel = mongoose.model('Currency', CurrencySchema);
+	logger.info(' • Seeding Currencies');
+	for (const { name, code } of currencyList) {
+		try {
+			const exist = await CurrencyModel.findOne({ code });
+			if (exist) {
+				logger.debug(`\t "${code}" already exists`);
+			} else {
+				await CurrencyModel.create({ name, code });
+				logger.info(`\t "${code}" added`);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
+};
