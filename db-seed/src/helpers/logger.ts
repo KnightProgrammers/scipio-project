324340833b736af3718e802f1c5c751ab4089ab6
Javier Caballero <caballerojavier13@gmail.com>
2023-10-04 19:00:10 +0000

    DB Seed: lint fix

--- db-seed/src/helpers/logger.ts
+++ db-seed/src/helpers/logger.ts
@@ -1,21 +1,21 @@
-import pino from 'pino';
-import pretty from 'pino-pretty';
+import pino from 'pino'
+import pretty from 'pino-pretty'
 
 const stream = pretty({
-	colorize: true,
-});
+    colorize: true,
+})
 
-const LOG_LEVELS = ['trace', 'debug', 'info', 'warn', 'error'];
+const LOG_LEVELS = ['trace', 'debug', 'info', 'warn', 'error']
 
-const logLevel: string = (process.env.LOG_LEVEL || '').toLowerCase();
+const logLevel: string = (process.env.LOG_LEVEL || '').toLowerCase()
 
 const logger = pino(
-	{
-		msgPrefix: '',
-		base: null,
-		level: LOG_LEVELS.includes(logLevel) ? logLevel : 'info',
-	},
-	stream,
-);
+    {
+        msgPrefix: '',
+        base: null,
+        level: LOG_LEVELS.includes(logLevel) ? logLevel : 'info',
+    },
+    stream,
+)
 
-export default logger;
+export default logger
