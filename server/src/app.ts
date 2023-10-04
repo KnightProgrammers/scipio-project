bfaddf424c99ba27e53aac9ea2425ca0655951fa
Javier Caballero <caballerojavier13@gmail.com>
2023-10-04 17:48:48 +0000

    Server: Add linter

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,58 +3,66 @@ import { join } from 'path';
 import AutoLoad from '@fastify/autoload';
 import cors from '@fastify/cors';
 import mongoose from 'mongoose';
-import { config } from "./config";
-
+import { config } from './config';
 
 // Pass --options via CLI arguments in command to enable these options.
 const options: any = {
-  logger: {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'mm/dd/yyyy HH:MM:ss',
-        ignore: 'pid,hostname,reqId',
-      },
-    },
-  }
-}
+	logger: {
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				translateTime: 'mm/dd/yyyy HH:MM:ss',
+				ignore: 'pid,hostname,reqId',
+			},
+		},
+	},
+};
 
-const app: any = async (
-  fastify: any,
-  opts: any
-): Promise<void> => {
+const app: any = async (fastify: any, opts: any): Promise<void> => {
+	fastify.log.info(
+		`${config.db.protocol}://${config.db.host}${
+			config.db.port ? `:${config.db.port}` : ''
+		}/${!!config.db.name && config.db.name}${
+			!!config.db.params && config.db.params
+		}`,
+	);
 
-  fastify.log.info(`${config.db.protocol}://${config.db.host}${config.db.port ? `:${config.db.port}` : ''}/${!!config.db.name && config.db.name}${!!config.db.params && config.db.params}`)
+	mongoose
+		.connect(
+			`${config.db.protocol}://${config.db.host}${
+				config.db.port ? `:${config.db.port}` : ''
+			}/${!!config.db.name && config.db.name}${
+				!!config.db.params && config.db.params
+			}`,
+			{
+				user: config.db.user,
+				pass: config.db.password,
+				autoCreate: true,
+			},
+		)
+		.then(() => fastify.log.info('MongoDB connected...'))
+		.catch((err) => fastify.log.error(err));
 
-  mongoose
-    .connect(`${config.db.protocol}://${config.db.host}${config.db.port ? `:${config.db.port}` : ''}/${!!config.db.name && config.db.name}${!!config.db.params && config.db.params}`, {
-      user: config.db.user,
-      pass: config.db.password,
-      autoCreate: true,
-    })
-    .then(() => fastify.log.info('MongoDB connected...'))
-    .catch(err => fastify.log.error(err));
+	await fastify.register(cors, () => (req: any, callback: any) => {
+		const corsOptions = {
+			// This is NOT recommended for production as it enables reflection exploits
+			origin: true,
+		};
 
-  await fastify.register(cors, () => (req: any, callback: any) => {
-    const corsOptions = {
-      // This is NOT recommended for production as it enables reflection exploits
-      origin: true
-    };
+		// do not include CORS headers for requests from localhost
+		if (/^localhost$/m.test(req.headers.origin)) {
+			corsOptions.origin = false;
+		}
 
-    // do not include CORS headers for requests from localhost
-    if (/^localhost$/m.test(req.headers.origin)) {
-      corsOptions.origin = false
-    }
+		// callback expects two parameters: error and options
+		callback(null, corsOptions);
+	});
 
-    // callback expects two parameters: error and options
-    callback(null, corsOptions)
-  });
-
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts
-  });
+	void fastify.register(AutoLoad, {
+		dir: join(__dirname, 'routes'),
+		options: opts,
+	});
 };
 
 export default app;
-export { app, options }
+export { app, options };
