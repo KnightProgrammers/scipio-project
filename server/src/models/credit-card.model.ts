a38c4462e0815768ef6ab401f0f840b8f1232a4e
JayC <caballerojavier13@gmail.com>
2023-10-09 22:09:13 -0300

    Server: Add model

---
+++ server/src/models/credit-card.model.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import { CurrencySchema } from "@/models/currency.model";
+
+export const BankSchema = new mongoose.Schema(
+	{
+		label: {
+			type: String,
+			required: true,
+		},
+		cardHolder: {
+			type: String,
+			required: false,
+		},
+		lastFourDigits: {
+			type: String,
+			required: true,
+		},
+		expiration: {
+			type: Date,
+			required: true,
+		},
+		billingCycle: {
+			type: String,
+			required: true,
+		},
+		issuer: {
+			type: String,
+			required: true,
+			enum : ['visa', 'mastercard', 'other'],
+			default: 'other'
+		},
+		status: {
+			type: String,
+			required: true,
+			enum : ['ACTIVE', 'EXPIRED', 'BLOCKED'],
+			default: 'ACTIVE'
+		},
+		creditLimitAmount: {
+			type: Number,
+			defaultValue: 0
+		},
+		creditLimitCurrency: {
+			type: CurrencySchema,
+		},
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		}
+	},
+	{ timestamps: true, skipVersioning: { dontVersionMe: true } },
+);
+
+export default mongoose.model('CreditCard', BankSchema);
