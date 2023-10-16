2f74b819aa889184f84df5d89cc0ff73210586ba
JayC <caballerojavier13@gmail.com>
2023-10-16 13:23:07 -0300

    E2E: New Test

--- tests/e2e-tests/src/specs/regression/configuration/credit-cards.spec.ts
+++ tests/e2e-tests/src/specs/regression/configuration/credit-cards.spec.ts
@@ -1,7 +1,9 @@
-import { Page, test } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
 import { v4 as uuidv4 } from "uuid";
 import { signInUser, signUpUser } from "../../../helpers/auth.helper";
 import firebaseService from "../../../services/firebase.service";
+import { waitForRequest } from "../../../helpers/generic.helper";
+import { NAV_MENU, navigateMenu } from "../../../helpers/nav-menu.helper";
 
 let email: string;
 let password: string;
@@ -35,7 +37,19 @@ test.afterAll(async () => {
     }
 });
 
-test('Empty state', async () => {})
+test('Empty state', async () => {
+    const waitForCreditCards = waitForRequest(page, 'userCreditCards');
+    const waitForUserCurrencies = waitForRequest(page, 'userCurrencies');
+    await navigateMenu(page, NAV_MENU.CREDIT_CARDS);
+    const creditCardsRequest = await waitForCreditCards;
+    await waitForUserCurrencies;
+    const creditCardsResponse = await creditCardsRequest.response();
+    const {data} = await creditCardsResponse.json();
+    expect(data.me.creditCards).toEqual([]);
+
+    const emptyStateContainer = page.locator('div[data-tn="empty-state-no-credit-cards"]');
+    await expect(emptyStateContainer).toBeVisible();
+})
 test('Add a credit card', async () => {})
 test('Edit a credit card', async () => {})
 test('delete a credit card', async () => {})
