26df13d65a5964da784eeee84ea163448bafbc78
Javier Caballero <caballerojavier13@gmail.com>
2023-10-19 16:36:27 -0300

    Infra: Tunning Sentry (#136)
    Signed-off-by: Javier Caballero <caballerojavier13@gmail.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-if (import.meta.env.VITE_FIREBASE_API_KEY) {
+if (import.meta.env.VITE_SENTRY_DSN) {
     Sentry.init({
-        dsn: import.meta.env.VITE_FIREBASE_API_KEY,
+        dsn: import.meta.env.VITE_SENTRY_DSN,
         integrations: [
             new Sentry.BrowserTracing({
                 // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
@@ -21,7 +21,7 @@ if (import.meta.env.VITE_FIREBASE_API_KEY) {
         // Performance Monitoring
         tracesSampleRate: 1.0, // Capture 100% of the transactions
         // Session Replay
-        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     })
 }
