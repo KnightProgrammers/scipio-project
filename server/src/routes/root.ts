f68bcad113532a1c407b07024b794ada11b5559a
Javier Caballero <caballerojavier13@gmail.com>
2023-10-19 16:26:45 +0000

    Infra: Add Sentry to the server

--- server/src/routes/root.ts
+++ server/src/routes/root.ts
@@ -1,9 +1,11 @@
+import { config } from '../config';
+
 const root: any = async (fastify: any): Promise<void> => {
 	fastify.get('/', async function (_: any, reply: any) {
 		reply.status(200).send({
 			app: 'Scipio',
-			version: process.env.npm_package_version,
-			environment: process.env.NODE_ENV || 'development',
+			version: config.app.version,
+			environment: config.app.environment
 		});
 	});
 };
