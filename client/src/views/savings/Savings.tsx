68f16e22e7bdfa4efe12a29b37a5dce870429a03
JayC <caballerojavier13@gmail.com>
2023-11-11 18:15:44 -0300

    Client: WIP

--- client/src/views/savings/Savings.tsx
+++ client/src/views/savings/Savings.tsx
@@ -1,5 +1,48 @@
+import { Button } from "@/components/ui";
+import { HiPlus } from "react-icons/hi";
+import { Container, Loading } from "@/components/shared";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 const Savings = () => {
-    return <div>Savings</div>
+    const [isFormOpen, setIsFormOpen] = useState(false)
+    const [selectedSaving, setSelectedSaving] = useState<any | undefined>(
+        undefined,
+    )
+    const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] =
+        useState<boolean>(false)
+
+    const { t } = useTranslation()
+
+    const loading = false;
+
+    if (loading) {
+        return (
+            <div className="flex h-full mx-auto w-0" data-tn="savings-page">
+                <Loading loading />
+            </div>
+        )
+    }
+
+    return (
+        <Container data-tn="savings-page">
+            <div className="lg:flex items-center justify-between mb-4">
+                <h2>{t('pages.savings.header')}</h2>
+                <div className="flex flex-col lg:flex-row lg:items-center">
+                    <Button
+                        variant="solid"
+                        size="sm"
+                        className="mt-4"
+                        icon={<HiPlus />}
+                        data-tn="add-category-btn"
+                        onClick={() => setIsFormOpen(true)}
+                    >
+                        {t('pages.savings.addSavingButton')}
+                    </Button>
+                </div>
+            </div>
+        </Container>
+    )
 }
 
 export default Savings
