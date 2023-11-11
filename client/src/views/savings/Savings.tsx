63af9cf0cd272be2c42894d55f831404d522bc0c
JayC <caballerojavier13@gmail.com>
2023-11-11 19:05:41 -0300

    Client: Add saving card

--- client/src/views/savings/Savings.tsx
+++ client/src/views/savings/Savings.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui";
-import { HiPlus } from "react-icons/hi";
-import { Container, Loading } from "@/components/shared";
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Button, Card, Dropdown, Progress } from "@/components/ui";
+import { HiCheckCircle, HiOutlinePencilAlt, HiOutlineTrash, HiPlus, HiXCircle } from "react-icons/hi";
+import { ConfirmDialog, Container, EllipsisButton, IconText, Loading } from "@/components/shared";
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
 const Savings = () => {
     const [isFormOpen, setIsFormOpen] = useState(false)
@@ -14,7 +14,15 @@ const Savings = () => {
 
     const { t } = useTranslation()
 
-    const loading = false;
+    const onDeleteConfirmClose = () => {
+
+    }
+
+    const onDelete = () => {
+
+    }
+
+    const loading = false
 
     if (loading) {
         return (
@@ -23,6 +31,8 @@ const Savings = () => {
             </div>
         )
     }
+
+    const savings: any[] = [{}, {}, {}]
 
     return (
         <Container data-tn="savings-page">
@@ -41,6 +51,97 @@ const Savings = () => {
                     </Button>
                 </div>
             </div>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                {savings.map((s: any, index: number) => (
+                    <Card key={index} bodyClass="flex flex-col justify-between h-full">
+                        <div className="flex justify-between">
+                            <h6>Saving #{index + 1}</h6>
+                            <Dropdown
+                                placement="bottom-end"
+                                renderTitle={
+                                    <EllipsisButton data-tn="dropdown-bank-account-btn" />
+                                }
+                            >
+                                <Dropdown.Item
+                                    eventKey="edit"
+                                    data-tn="edit-saving-btn"
+                                    onClick={() => {
+                                        setSelectedSaving(s)
+                                        setIsFormOpen(true)
+                                    }}
+                                >
+                                    <IconText
+                                        className="text-sm font-semibold w-full"
+                                        icon={
+                                            <HiOutlinePencilAlt />
+                                        }
+                                    >
+                                        {t('actions.edit')}
+                                    </IconText>
+                                </Dropdown.Item>
+                                <Dropdown.Item
+                                    eventKey="delete"
+                                    data-tn="delete-saving-btn"
+                                    onClick={() => {
+                                        setSelectedSaving(s)
+                                        setIsConfirmDeleteOpen(true)
+                                    }}
+                                >
+                                    <IconText
+                                        className="text-red-400 hover:text-red-600 text-sm font-semibold w-full"
+                                        icon={
+                                            <HiOutlineTrash />
+                                        }
+                                    >
+                                        {t('actions.delete')}
+                                    </IconText>
+                                </Dropdown.Item>
+                            </Dropdown>
+                        </div>
+                        <p className="mt-4">
+                            It is not about bits, bytes and protocols, but profits, losses and margins.
+                        </p>
+                        <div className="mt-4">
+                            <Progress
+                                color="green-500"
+                                percent={Math.round(Math.random() * 100)}
+                                customInfo={
+                                    (Math.round(Math.random()))
+                                        ? <HiCheckCircle className="text-emerald-500 text-xl" />
+                                        : <HiXCircle className="text-red-500 text-xl" />
+                                }
+                            />
+                            <div className="flex items-center justify-between mt-2">
+                                <div className="flex items-center rounded-full font-semibold text-xs">
+                                    <div className="flex items-center px-2 py-1 border border-gray-300 rounded-full">
+
+                                        <span className="ml-1 rtl:mr-1 whitespace-nowrap">27 / 32</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                ))}
+            </div>
+            <ConfirmDialog
+                isOpen={isConfirmDeleteOpen}
+                type="danger"
+                title={t('pages.savings.deleteConfirmation.title')}
+                data-tn="confirm-saving-account-dialog"
+                confirmButtonColor="red-600"
+                confirmText={t('actions.delete')}
+                cancelText={t('actions.cancel')}
+                onClose={onDeleteConfirmClose}
+                onRequestClose={onDeleteConfirmClose}
+                onCancel={onDeleteConfirmClose}
+                onConfirm={onDelete}
+            >
+                <p>
+                    {t('pages.savings.deleteConfirmation.description', {
+                        saving: selectedSaving?.name,
+                    })}
+                </p>
+            </ConfirmDialog>
         </Container>
     )
 }
