81dfa9041866916d86c72844a263715b09480880
Javier Caballero <caballerojavier13@gmail.com>
2023-11-30 20:24:28 +0000

    Server: Update services

--- server/src/services/expense.service.ts
+++ server/src/services/expense.service.ts
@@ -32,7 +32,7 @@ class ExpenseService {
 				},
 				isDeleted: false
 			})
-			.sort({ billableDate: 1 });
+			.sort({ billableDate: -1 });
 	}
 	static async getAllByCategory(userId: string, categoryId: string, fromDate?: string, toDate?: string) {
 		const formattedFromDate = parseFilterDate(fromDate, 1);
@@ -48,7 +48,7 @@ class ExpenseService {
 				},
 				isDeleted: false
 			})
-			.sort({ billableDate: 1 });
+			.sort({ billableDate: -1 });
 	}
 	static async getAllByCreditCard(userId: string, creditCardId: string, fromDate?: string, toDate?: string) {
 		const formattedFromDate = parseFilterDate(fromDate, 1);
@@ -64,9 +64,8 @@ class ExpenseService {
 				},
 				isDeleted: false
 			})
-			.sort({ billableDate: 1 });
+			.sort({ billableDate: -1 });
 	}
-
 	static async getAllByCurrency(userId: string, currencyId: string, fromDate?: string, toDate?: string) {
 		const formattedFromDate = parseFilterDate(fromDate, 1);
 		const formattedToDate = parseFilterDate(toDate, 32);
@@ -81,7 +80,29 @@ class ExpenseService {
 				},
 				isDeleted: false
 			})
-			.sort({ billableDate: 1 });
+			.sort({ billableDate: -1 });
+	}
+	static async getAllByCreditCardMonthlyStatement(userId: string, creditCardMonthlyStatementId: string) {
+		return ExpenseModel
+			.find({
+				userId,
+				creditCardMonthlyStatementId,
+				isDeleted: false
+			})
+			.sort({ billableDate: -1 });
+	}
+	static async getAllWithoutCreditCardMonthlyStatement(userId: string, creditCardId: string, toDate: Date) {
+		return ExpenseModel
+			.find({
+				userId,
+				creditCardId,
+				billableDate: {
+					$lte: toDate
+				},
+				creditCardMonthlyStatementId: { $exists: false },
+				isDeleted: false
+			})
+			.sort({ billableDate: -1 });
 	}
 	static async create(userId: string, data: ExpenseInput) {
 		const {
