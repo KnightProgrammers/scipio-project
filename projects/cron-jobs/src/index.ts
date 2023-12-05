e0a951ed898952a63c54f1e3e4e84108b7d03c00
Javier Caballero <caballerojavier13@gmail.com>
2023-12-05 23:09:18 +0000

    Feat: Job to auto-expire savings

--- projects/cron-jobs/src/index.ts
+++ projects/cron-jobs/src/index.ts
@@ -1,5 +1,6 @@
 import * as cron from 'node-cron';
 import { creditCardExpirationJob } from './jobs/credit-card-expiration.job';
+import { savingExpirationJob } from './jobs/saving-expiration.job';
 
 // Website to create cron expressions
 // https://crontab.cronhub.io/
@@ -7,4 +8,9 @@ import { creditCardExpirationJob } from './jobs/credit-card-expiration.job';
 // Every Day at 00:00AM
 cron.schedule(`0 0 * * *`, async () => {
   await creditCardExpirationJob();
+});
+
+// Every Day at 00:00AM
+cron.schedule(`0 0 * * *`, async () => {
+  await savingExpirationJob();
 });
