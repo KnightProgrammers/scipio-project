adb299b23484e9e8cdc7cd79aef92873bc262771
Javier Caballero <caballerojavier13@gmail.com>
2023-12-05 20:32:26 -0300

    Feat: [SP-114] Auto expire savings job (#230)

---
+++ projects/cron-jobs/src/jobs/saving-expiration.job.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import { DateTime } from 'luxon';
+import { SavingSchema } from '@knightprogrammers/scpio-db-schemas';
+import getLogger from '../helpers/logger';
+
+dotenv.config();
+
+export const savingExpirationJob = async (
+  limit: number = 100
+): Promise<void> => {
+  const logger = getLogger('[SavingExpiration] ');
+  const dbConnection = await mongoose.connect(process.env.MONGO_DB_URI || '');
+  const SavingModel = mongoose.model('Saving', SavingSchema);
+  try {
+    logger.info('Starting job');
+    const savings: any[] = await SavingModel.find({
+      status: 'IN_PROGRESS',
+      targetDate: {
+        $lte: DateTime.now().toJSDate(),
+      },
+    }).limit(limit);
+    for (const saving of savings) {
+      saving.status = 'EXPIRED';
+      await saving.save();
+    }
+    logger.info(
+      `${savings.length} saving${savings.length > 1 ? 's' : ''} expired`
+    );
+  } catch (e: any) {
+    logger.debug(e);
+    logger.error('Error expiring savings');
+  }
+  await dbConnection.disconnect();
+};
