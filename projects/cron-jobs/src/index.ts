8510c9d7c5cddb3fec176137d03d49d2892c244f
Javier Caballero <caballerojavier13@gmail.com>
2023-12-05 19:01:39 +0000

    Chore: Update Cron

--- projects/cron-jobs/src/index.ts
+++ projects/cron-jobs/src/index.ts
@@ -1,6 +1,10 @@
 import * as cron from 'node-cron';
 import { creditCardExpirationJob } from './jobs/credit-card-expiration.job';
+
+// Website to create cron expressions
+// https://crontab.cronhub.io/
  
-cron.schedule(`*/1 * * * *`, async () => {
+// Every Day at 00:00AM
+cron.schedule(`0 0 * * *`, async () => {
   await creditCardExpirationJob();
 });
