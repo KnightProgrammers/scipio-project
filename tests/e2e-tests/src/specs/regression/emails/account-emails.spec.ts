9eb02c9a15ed2ca76125cb42fc6656c42b3b80f3
JayC <caballerojavier13@gmail.com>
2023-12-09 23:26:35 -0300

    Maint: Update tests

--- tests/e2e-tests/src/specs/regression/emails/account-emails.spec.ts
+++ tests/e2e-tests/src/specs/regression/emails/account-emails.spec.ts
@@ -1,12 +1,12 @@
 import { Page, expect, test} from "@playwright/test";
 import firebaseService from '../../../services/firebase.service';
 import {
-    DEFAULT_USER_COUNTRY, DEFAULT_USER_CURRENCIES,
-    DEFAULT_USER_LANG,
+    DEFAULT_USER_COUNTRY,
+    DEFAULT_USER_CURRENCIES, logoutUser,
     signInUser,
     signUpUser,
     welcomeUser
-} from '../../../helpers/auth.helper';
+} from "../../../helpers/auth.helper";
 import { v4 as uuidv4 } from 'uuid';
 
 import mailhog = require("mailhog");
@@ -22,10 +22,10 @@ const LANG_CONFIG: any[] = [
         name: 'EN',
         expects: {
             welcomeEmail: {
-                emailSubject: '',
+                subject: 'Welcome to Scipio',
             },
             resetPasswordEmail: {
-                emailSubject: '',
+                subject: 'Reset Password',
             }
         }
     },
@@ -34,10 +34,10 @@ const LANG_CONFIG: any[] = [
         name: 'ES',
         expects: {
             welcomeEmail: {
-                emailSubject: '',
+                subject: 'Bienvenido/a a Scipio',
             },
             resetPasswordEmail: {
-                emailSubject: '',
+                subject: 'Restablecer Contraseña',
             }
         }
     }
@@ -63,11 +63,11 @@ for (const lang of LANG_CONFIG) {
 
         test.afterAll(async () => {
             try {
-                // const user = await  firebaseService.auth().getUserByEmail(email);
-                // if (user) {
-                //     await firebaseService.auth().deleteUser(user.uid);
-                //     console.log(`Deleted User "${email}"`);
-                // }
+                const user = await  firebaseService.auth().getUserByEmail(email);
+                if (user) {
+                    await firebaseService.auth().deleteUser(user.uid);
+                    console.log(`Deleted User "${email}"`);
+                }
             } catch {
                 console.log('No user to be deleted');
             } finally {
@@ -75,22 +75,37 @@ for (const lang of LANG_CONFIG) {
             }
         });
         test('Welcome Email', async () => {
-            // await signUpUser(page, { email, password, name });
-            // login
-            // welcome: LANG = lang.name
+            await signUpUser(page, { email, password, name });
+            await signInUser(page, { email, password }, false);
+            await welcomeUser(page, {
+                lang: lang.name.toLowerCase(),
+                country: DEFAULT_USER_COUNTRY,
+                currencies: DEFAULT_USER_CURRENCIES
+            })
+            await page.waitForTimeout(3000);
             // Check welcome Email
-            const result = await mailhogClient.latestTo("test@scipio.com");
-            // Validate result is not undefined/null
-            console.log('ID: ', result.ID) // Forgot password email id is different that welcome email id
-            console.log('From: ', result.from)
-            console.log('To: ', result.to)
-            console.log('Subject: ', result.subject)
+            const result = await mailhogClient.latestTo(email);
+            expect(result).toBeDefined();
+            expect(result.from).toEqual('no-reply@scipiofinances.com')
+            expect(result.to).toEqual(email)
+            expect(result.subject).toEqual(lang.expects.welcomeEmail.subject)
+            await mailhogClient.deleteMessage(result.ID);
         });
 
         test('Forgot Password Email', async () => {
-            // Log off
+            await logoutUser(page);
             // Forgot Password
-            // Validate Welcome email
+            await expect(page.locator('div[data-tn="sign-in-page"]')).toBeVisible();
+            await page.locator('a[data-tn="forgot-password-link"]').click()
+            await page.locator('input[name="email"]').fill(email);
+            await page.locator('button[data-tn="send-email-btn"]').click();
+            // Validate reset password email
+            const result = await mailhogClient.latestTo(email);
+            expect(result).toBeDefined();
+            expect(result.from).toEqual('no-reply@scipiofinances.com')
+            expect(result.to).toEqual(email)
+            expect(result.subject).toEqual(lang.expects.welcomeEmail.subject)
+            await mailhogClient.deleteMessage(result.ID);
         });
     })
 }
