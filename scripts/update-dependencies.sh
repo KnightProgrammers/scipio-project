73253c8dbf806dd6b02d039a06dc72c91f921d28
Javier Caballero <caballerojavier13@gmail.com>
2023-12-13 11:07:51 -0300

    Maint: Update dependencies (#248)

---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+declare -A projects=(
+    ["graphql-server"]="/projects/graphql-server"
+    ["db-seed"]="/projects/db-seed"
+    ["cron-jobs"]="/projects/cron-jobs"
+    ["client"]="/projects/client"
+    ["infra-lib"]="/packages/infra"
+    ["db-schemas-lib"]="/packages/db-schemas"
+)
+
+# Get root directory
+root_dir="$(git rev-parse --show-toplevel)"
+
+# Install the latest version of npm-check-updates
+npm i -g npm-check-updates
+
+# Checkout to a new branch
+git checkout -b update-dependencies
+
+# For every npm project
+for key in "${!projects[@]}"; do
+  printf "%s\n" "$key"
+    # Chenge directory
+    cd "${root_dir}/${projects[$key]}"
+    # Check of updates
+    npm-check-updates -u
+    # Install Updates
+    npm i
+    # Commit Changes
+    git commit -am "Maint: Update dependencies for $key" # includes project name
+done
