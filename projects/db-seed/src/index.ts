ff358093fc98f9973073bbba3fa3b3fe89aac6c9
Javier Caballero <caballerojavier13@gmail.com>
2023-12-19 16:50:54 +0000

    Chore: Fix env name

--- projects/db-seed/src/index.ts
+++ projects/db-seed/src/index.ts
@@ -4,24 +4,17 @@ import logger from './helpers/logger';
 import countrySeed from './seeds/country';
 import currencySeed from './seeds/currency';
 
-console.log(process.env.MONGO_DB_URI)
+dotenv.config();
+
+const config = {
+	db: {
+		uri: process.env.MONGO_DB_URI || '',
+	},
+}
 
 ;(async function main() {
-
-	console.log(process.env.MONGO_DB_URI)
-
-	dotenv.config();
-	
-	const config = {
-		db: {
-			uri: process.env.MONGO_DB_URI || '',
-		},
-	}
-	console.log(process.env.MONGO_DB_URI)
-
 	logger.info('Connecting to DB');
 	logger.info(config.db.uri);
-
 	const dbConnection = await mongoose.connect(config.db.uri);
 	logger.info('DB connection is up');
 	logger.info('-----------------');
